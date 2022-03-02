92a5fa0839d73d8f3b18366616a80f7fecc49a84
Raul <61325276+KeywizZ@users.noreply.github.com>
2022-03-02 17:47:46 +0100

    Iteration#2

---
+++ iteration2.js
@@ -0,0 +1,46 @@
+// Mix Fors -- Media del volumen de todos los usuarios
+
+const users = [
+    {name: 'Manolo el del bombo',
+        favoritesSounds: {
+            waves: {format: 'mp3', volume: 50},
+            rain: {format: 'ogg', volume: 60},
+            firecamp: {format: 'mp3', volume: 80},
+        }
+    },
+    {name: 'Mortadelo',
+        favoritesSounds: {
+            waves: {format: 'mp3', volume: 30},
+            shower: {format: 'ogg', volume: 55},
+            train: {format: 'mp3', volume: 60},
+        }
+    },
+    {name: 'Super Lopez',
+        favoritesSounds: {
+            shower: {format: 'mp3', volume: 50},
+            train: {format: 'ogg', volume: 60},
+            firecamp: {format: 'mp3', volume: 80},
+        }
+    },
+    {name: 'El culebra',
+        favoritesSounds: {
+            waves: {format: 'mp3', volume: 67},
+            wind: {format: 'ogg', volume: 35},
+            firecamp: {format: 'mp3', volume: 60},
+        }
+    },
+]
+
+let suma = 0;
+let total = 0;
+
+for (const user of users) {
+    for (const key in user.favoritesSounds) {
+        suma += user.favoritesSounds[key].volume;
+        total++;
+    }
+}
+let media;
+media = suma/total;
+
+console.log(media)
